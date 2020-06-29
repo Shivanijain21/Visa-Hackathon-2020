4ba09937f5524ec1983b3b8bb1c71cdf3ab6b4a2
anand-saurabh <saurabh.anand@ufl.edu>
2020-06-29 18:15:40 -0400

    changes and coee refactor

--- funds_transfer_api/src/main/java/com/visa/developer/sample/funds_transfer_api/controller/RestController.java
+++ funds_transfer_api/src/main/java/com/visa/developer/sample/funds_transfer_api/controller/RestController.java
@@ -19,7 +19,7 @@ import org.threeten.bp.ZoneId;
 import org.threeten.bp.ZonedDateTime;
 import org.threeten.bp.format.DateTimeFormatter;
 import org.threeten.bp.temporal.ChronoUnit;
-
+import java.util.Base64;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.security.SecureRandom;
@@ -31,7 +31,7 @@ public class RestController {
     @Autowired
     FundTransferService fundTransferService;
 
-    private  FundsTransferApi api = null;
+    private FundsTransferApi api = null;
 
 
     @RequestMapping(value = "/fund", method = RequestMethod.POST)
@@ -40,21 +40,20 @@ public class RestController {
     }
 
 
-
     @PostConstruct
     void init() {
         ApiClient apiClient = new ApiClient();
         Config config = new Config();
         apiClient.setUsername(config.getProperty("userName"));
-        apiClient.setPassword(config.getProperty("password"));
-        apiClient.setKeystorePath(config.getResourceFilePath(config.getProperty("keyStorePath")));
-        //apiClient.setKeystorePath("C:\\\\study\\\\summer_code\\\\visa_direct\\\\funds_transfer_api\\\\src\\\\main\\\\resources\\\\myProject_keyAndCertBundle.jks");
+        apiClient.setPassword(decodePassword(config.getProperty("password")));
+        //apiClient.setKeystorePath(config.getResourceFilePath(config.getProperty("keyStorePath")));
+        apiClient.setKeystorePath("./myProject_keyAndCertBundle.jks");
 
         apiClient.setKeystorePassword(config.getProperty("keyStorePassword"));
         apiClient.setPrivateKeyPassword(config.getProperty("keyStorePassword"));
         // If behind a proxy set proxy host and port numbers here
         String proxyHostName = config.getProperty("proxyHostName");
-        if(!StringUtils.isEmpty(proxyHostName)){
+        if (!StringUtils.isEmpty(proxyHostName)) {
             apiClient.setProxyHostName(proxyHostName);
             apiClient.setProxyPortNumber(Integer.parseInt(config.getProperty("proxyPortNumber")));
         }
@@ -73,6 +72,7 @@ public class RestController {
 
         api = new FundsTransferApi(apiClient);
     }
+
     public boolean getpullfundstransactions(String token) throws IOException {
         String jsonPayload = transformPayload("{\"localTransactionDateTime\": \"2016-11-16T23:33:06\", \"businessApplicationId\": \"AA\", \"cpsAuthorizationCharacteristicsIndicator\": \"Y\", \"senderCardExpiryDate\": \"2015-10\", \"amount\": \"124.02\", \"acquirerCountryCode\": \"840\", \"retrievalReferenceNumber\": \"330000550000\", \"cardAcceptor\": {\"idCode\": \"ABCD1234ABCD123\", \"address\": {\"county\": \"081\", \"country\": \"USA\", \"state\": \"CA\", \"zipCode\": \"94404\"}, \"terminalId\": \"ABCD1234\", \"name\": \"Visa Inc. USA-Foster City\"}, \"acquiringBin\": \"408999\", \"systemsTraceAuditNumber\": \"451001\", \"nationalReimbursementFee\": \"11.22\", \"senderCurrencyCode\": \"USD\", \"cavv\": \"0700100038238906000013405823891061668252\", \"foreignExchangeFeeTransaction\": \"11.99\", \"addressVerificationData\": {\"postalCode\": \"12345\", \"street\": \"XYZ St\"}, \"senderPrimaryAccountNumber\": \"4895142232120006\", \"surcharge\": \"11.99\"}");
         ObjectMapper mapper = new ObjectMapper();
@@ -85,22 +85,20 @@ public class RestController {
             }, String.class, true);
 
             PullfundstransactionsgetResponse response = api.getpullfundstransactions(statusIdentifier);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             ex.getStackTrace();
             return false;
         }
         return true;
     }
 
-    public String transformPayload(String payload) {
+    private String transformPayload(String payload) {
         payload = editLocalTime(payload);
         payload = addRandomValue(payload);
         return payload;
     }
 
-    public String editLocalTime(String payload) {
+    private String editLocalTime(String payload) {
         ZoneId zoneId = ZoneId.of("America/Los_Angeles");
         LocalDateTime localDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
         ZonedDateTime laDateTime = ZonedDateTime.of(localDateTime, zoneId);
@@ -111,7 +109,7 @@ public class RestController {
         return payload;
     }
 
-    public String addRandomValue(String payload) {
+    private String addRandomValue(String payload) {
         String randomAlphaNumericValue = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 8);
         SecureRandom rand = new SecureRandom();
         Integer randomNumber = rand.nextInt(90000) + 10000;
@@ -121,5 +119,10 @@ public class RestController {
         return payload;
     }
 
-
+    private String decodePassword(String text) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        // Decoding string
+        String dStr = new String(decoder.decode(text));
+        return dStr;
+    }
 }
