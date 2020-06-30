863d446902b140dd24972a28e8b5a3c0d68233dd
Mikhail Husyev <mikhail@MacBook-Pro.local>
2020-06-30 01:37:13 -0500

    Created Component

--- front-end/Components/Security/SensitiveStorage.js
+++ front-end/Components/Security/SensitiveStorage.js
@@ -1,28 +1,20 @@
 import SInfo from "react-native-sensitive-info";
-import { Component } from "react";
+import React from "react";
 
-const setSensitiveStorage = async () => {
-  SInfo.setItem("key1", "value1", {
-    sharedPreferencesName: "mySharedPrefs",
-    keychainService: "myKechain",
-  });
-};
+class SensitiveStorage extends React.Component {
+  setSensitiveData(key, value) {
+    SInfo.setItem(key, value, {
+      sharedPreferencesName: "mySharedPrefs",
+      keychainService: "myKeychain",
+    });
+  }
 
-class SensitiveStorage extends Component(){
-    setSensitiveData(key, value) {
-        SInfo.setItem(key, value, {
-            sharedPreferencesName: "mySharedPrefs",
-            keychainService: "myKeychain",
-          }); 
-    }
-
-    getSensitiveData(key, value){
-        SInfo.setItem(key, value, {
-            sharedPreferencesName: "mySharedPrefs",
-            keychainService: "myKeychain"
-        })
-    }
-
+  getSensitiveData(key, value) {
+    SInfo.setItem(key, value, {
+      sharedPreferencesName: "mySharedPrefs",
+      keychainService: "myKeychain",
+    });
+  }
 }
 
-export default function SensitiveStorage();
+export default SensitiveStorage;
