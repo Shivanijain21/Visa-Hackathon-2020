fb60c1087d4383f76723d5cb3792b2af24d9d226
Johan Guzman <jguzm022@ucr.edu>
2020-06-30 11:54:37 -0700

    Changes transaction flow by adding two types of buttons to start a transaction and to submit a transaction

--- front-end/Screens/Transaction/SubmitButton.js
+++ front-end/Screens/Transaction/SubmitButton.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-native-paper';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator, Text } from 'react-native';
 import ScreenNames from '../Names';
+import { Modal, Portal } from 'react-native-paper';
 
 const styles = StyleSheet.create({
 	Container: {
@@ -11,9 +12,61 @@ const styles = StyleSheet.create({
 	Text: {
 		fontSize: 30,
 	},
+	modalContainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
 
-export default function ({ amount, setBusy, navigate }) {
+export default function ({ amount, navigate, setAmount }) {
+	// Transaction states
+	const startTransactionState = {
+		state: true,
+		waitingText: "Waiting for customer's token ...",
+	};
+
+	const sumbitTransactionState = {
+		state: false,
+		waitingText: 'Submitting transaction ...',
+	};
+
+	const [transactionState, setTransactionState] = useState({
+		...startTransactionState,
+	});
+	const [busy, setBusy] = useState(false);
+
+	const startTransaction = () => {
+		// *****************************************************
+		// TO DO:  ENTER CODE TO RECEIVE TOKEN USING NFC Manager
+		// *****************************************************
+		setBusy(true);
+		setTimeout(() => {
+			setBusy(false);
+			// Change transaction state to submit payment
+			setTransactionState({
+				...sumbitTransactionState,
+			});
+		}, 3000);
+	};
+
+	const submitTransaction = () => {
+		// ***********************************************************************
+		// ENTER AXIOS CALL TO OUR BACKEND SERVICE WHICH WILL CALL VISA DIRECT API
+		// ***********************************************************************
+		setBusy(true);
+		setTimeout(() => {
+			setBusy(false);
+			// Clear the amount
+			setAmount('');
+			// Change transaction state back to start transaction
+			setTransactionState({
+				...startTransactionState,
+			});
+			// Navigate to the success page
+			navigate(ScreenNames.TransactionSuccessScreen, { amount });
+		}, 3000);
+	};
+
 	const isAmountValid = () => {
 		const [wholePart, decimalPart] = amount.split('.');
 
@@ -33,29 +86,48 @@ export default function ({ amount, setBusy, navigate }) {
 		return false;
 	};
 
-	const submitTransaction = () => {
-		// Here we will call our backend service which will then call VISA DIRECT API
-		console.log('Submitting Transaction ...');
-
-		// Set busy state for 3 seconds
-		setBusy(true);
-		setTimeout(() => {
-			setBusy(false);
-			navigate(ScreenNames.TransactionSuccessScreen, { amount });
-		}, 3000);
-	};
-
 	return (
 		<View style={{ flex: 1 }}>
-			<Button
-				contentStyle={styles.Container}
-				labelStyle={styles.Text}
-				mode='contained'
-				onPress={() => submitTransaction()}
-				disabled={!isAmountValid()}
-			>
-				SUBMIT PAYMENT
-			</Button>
+			{transactionState.state ? (
+				<Button
+					contentStyle={styles.Container}
+					labelStyle={styles.Text}
+					mode='contained'
+					onPress={() => startTransaction()}
+					disabled={!isAmountValid()}
+				>
+					START TRANSACTION
+				</Button>
+			) : (
+				<Button
+					contentStyle={styles.Container}
+					labelStyle={styles.Text}
+					mode='contained'
+					onPress={() => submitTransaction()}
+					disabled={!isAmountValid()}
+				>
+					SUBMIT PAYMENT
+				</Button>
+			)}
+			<Portal>
+				<Modal
+					visible={busy}
+					dismissable={false}
+					contentContainerStyle={styles.modalContainer}
+				>
+					<ActivityIndicator size={170} color='#262D9B' />
+					<Text
+						style={{
+							color: '#262D9B',
+							marginTop: 30,
+							fontSize: 25,
+							fontWeight: 'bold',
+						}}
+					>
+						{transactionState.waitingText}
+					</Text>
+				</Modal>
+			</Portal>
 		</View>
 	);
 }
