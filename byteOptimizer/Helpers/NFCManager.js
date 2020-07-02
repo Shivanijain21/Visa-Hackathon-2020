e72f47a5868f0ea3115b5a7d92993ac14e5923f6
Shivanijain21 <shivani.jain@sjsu.edu>
2020-07-01 18:04:35 -0700

    reading from nfc

--- byteOptimizer/Helpers/NFCManager.js
+++ byteOptimizer/Helpers/NFCManager.js
@@ -2,28 +2,32 @@ import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
 import { NativeModules } from 'react-native';
 
 function setupNFC() {
-  console.log("****************-------in setupnfc func---------***************");
+  //console.log("****************-------in setupnfc func---------***************");
   NfcManager.start();
-  console.log("****************-------after start---------***************");
+  //console.log("****************-------after start---------***************");
   NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
-    console.log('*********tag***********\n', tag);
-    console.log(tag["ndefMessage"][0].payload);
+    //console.log('*********tag***********\n', tag);
+    //console.log(tag["ndefMessage"][0].payload);
     let pay = tag["ndefMessage"][0].payload;
-    let res = ""
-    // pay.forEach((item) => {
-    //   res += (char)item;
-    // });
-    console.log("printing pay::   " + pay);
-    // NfcManager.setAlertMessageIOS('I got your tag!');
+    pay = pay.slice(3, pay.length)
+    pay = String.fromCharCode(...pay)
+    console.log("********-------START-------*********\n")
+    console.log(pay + "\n")
+    console.log("********-------END---------*********\n")
+    console.log("Printing pay::   " + pay);
+    NfcManager.setAlertMessageIOS('I got your tag!');
     NfcManager.unregisterTagEvent().catch(() => 0);
-    //return tag;
+    return pay;
   });
 }
 
 export default readTag = async () => {
-  await setupNFC();
+  // await setupNFC();
   try {
-    await NfcManager.registerTagEvent();
+    console.log("from register tag");
+    let data = await NfcManager.registerTagEvent();
+    console.log("********-------data---------*********\n")
+    console.log(data);
   } catch (ex) {
     console.warn('ex', ex);
     NfcManager.unregisterTagEvent().catch(() => 0);
--- byteOptimizer/Screens/Transaction/SubmitButton.js
+++ byteOptimizer/Screens/Transaction/SubmitButton.js
@@ -3,6 +3,8 @@ import { Button } from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
 import ScreenNames from '../Names';
 import readTag from '../../Helpers/NFCManager';
+import NfcManager, { NfcEvents } from 'react-native-nfc-manager';
+
 
 const styles = StyleSheet.create({
 	Container: {
@@ -33,19 +35,53 @@ export default function ({ amount, setBusy, navigate }) {
 		}
 		return false;
 	};
+	function setupNFC() {
+		//console.log("****************-------in setupnfc func---------***************");
+		// NfcManager.start();
+		//console.log("****************-------after start---------***************");
+		NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+			//console.log('*********tag***********\n', tag);
+			//console.log(tag["ndefMessage"][0].payload);
+			let pay = tag["ndefMessage"][0].payload;
+			pay = pay.slice(3, pay.length)
+			pay = String.fromCharCode(...pay)
+			console.log("********-------START-------*********\n")
+			console.log(pay + "\n")
+			console.log("********-------END---------*********\n")
+			console.log("Printing pay::   " + pay);
+			NfcManager.setAlertMessageIOS('I got your tag!');
+			NfcManager.unregisterTagEvent().catch(() => 0);
+			let data = {
+				token: pay,
+				amount: { amount }
+			}
+			console.log(data);
+			setBusy(true);
+			setTimeout(() => {
+				setBusy(false);
+				navigate(ScreenNames.TransactionSuccessScreen, { amount });
+			}, 3000);
+		});
+	};
 
 	const submitTransaction = async () => {
 		// Here we will call our backend service which will then call VISA DIRECT API
 		console.log('Submitting Transaction ...');
 		console.log("reading");
-		let data = readTag();
-		console.log(data);
+
+		await setupNFC();
+		try {
+			console.log("from register tag");
+			await NfcManager.registerTagEvent();
+			// console.log("********-------data---------*********\n")
+			// console.log(data);
+		} catch (ex) {
+			console.warn('ex', ex);
+			NfcManager.unregisterTagEvent().catch(() => 0);
+		}
+		// console.log(data);
 		// Set busy state for 3 seconds
-		setBusy(true);
-		setTimeout(() => {
-			setBusy(false);
-			navigate(ScreenNames.TransactionSuccessScreen, { amount });
-		}, 3000);
+
 	};
 
 	return (
